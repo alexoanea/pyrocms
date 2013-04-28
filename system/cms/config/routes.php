92161cb5f8b138076f91ff79de7d97fede7d78e9
Alex Oanea <contact@alexoanea.com>
2013-04-28 07:13:23 +0100

    Change default htaccess and routes for my setup

--- system/cms/config/routes.php
+++ system/cms/config/routes.php
@@ -38,13 +38,22 @@
 |
 */
 
-$route['default_controller']                = 'pages';
 $route['404_override']                      = 'pages';
 
-$route['admin/help/([a-zA-Z0-9_-]+)']       = 'admin/help/$1';
-$route['admin/([a-zA-Z0-9_-]+)/(:any)']	    = '$1/admin/$2';
-$route['admin/(login|logout|remove_installer_directory)']			    = 'admin/$1';
-$route['admin/([a-zA-Z0-9_-]+)']            = '$1/admin/index';
+// Admin routes
+$route['admin/?'] = 'admin';
+$route['admin/help/([a-zA-Z0-9_-]+)'] = 'admin/help/$1';
+$route['admin/([a-zA-Z0-9_-]+)/(:any)']	= '$1/admin/$2';
+$route['admin/(login|logout)'] = 'admin/$1';
+$route['admin/([a-zA-Z0-9_-]+)'] = '$1/admin/index';
+
+$route['default_controller']                = 'pages';
+
+
+// $route['admin/help/([a-zA-Z0-9_-]+)']       = 'admin/help/$1';
+// $route['admin/([a-zA-Z0-9_-]+)/(:any)']	    = '$1/admin/$2';
+// $route['admin/(login|logout|remove_installer_directory)']			    = 'admin/$1';
+// $route['admin/([a-zA-Z0-9_-]+)']            = '$1/admin/index';
 
 $route['api/ajax/(:any)']          			= 'api/ajax/$1';
 $route['api/([a-zA-Z0-9_-]+)/(:any)']	    = '$1/api/$2';
